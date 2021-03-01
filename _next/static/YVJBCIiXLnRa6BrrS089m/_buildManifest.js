46b18b83bd59c12050a91505520713d0f5c16fcd
Jason Sturges <jason@jasonsturges.com>
2021-03-01 01:15:22 -0600

    gh-pages

---
+++ _next/static/YVJBCIiXLnRa6BrrS089m/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-3c982e55dcbca1c4da57.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8aff84eeec66a7924a22.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
